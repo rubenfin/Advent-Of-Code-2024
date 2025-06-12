b320056bdf865e85206bf918072fb773505db524
rubenfin <rubenkfinnerud@gmail.com>
2025-06-12 18:59:21 +0200

    day7ex1 start

--- day7/1/main.py
+++ day7/1/main.py
@@ -1,23 +1,56 @@
-from guard import Guard
+def do_operations(values: list, operations: list):
+    result = 0
+    stack = values.copy()
+    stack.reverse()
+    for i in range(0, len(operations)):
+        num1 = stack.pop()
+        num2 = stack.pop()
+        print(num1, operations[i], num2)
+        if operations[i] == '+':
+            stack.append(num1+num2)
+        else:
+            stack.append(num1*num2)
+            print(num1*num2)
+    print("RESULT", stack[0])
+    return stack[0]
 
-def find_guard(mmap: list, height: int, width: int)-> list:
-    for y in range(0, height):
-        for x in range(0, width):
-            if mmap[y][x] in '^>v<':
-                return (y, x)
+def calculate(result: int, values: list):
+    length = len(values)
+    operations = ['*'] * (length - 1)
+    accepted = [0] * length
+    for i in range(0, length):
+        for j in range(0, operations):
+            for x in range(0, operations):
+                res = do_operations(values, operations)
+                if res == result:
+                    accepted[i] = 1
+    return accepted
+
+def go_through_numbers(results: list, arr: list):
+    idx = 0
+    for idx in range(0, len(results)):
+        calculate(results[idx], arr[idx])
 
 def main():
-    mmap = []
     with open("../input.txt") as f:
-        mmap = f.read().split('\n')
-    mmap = [list(row) for row in mmap]
-    height = len(mmap)
-    width = len(mmap[0])
-    g_location = find_guard(mmap, height, width)
-    y, x = g_location[0], g_location[1]
-    g = Guard(mmap[y][x], g_location,height, width)
-    g.move(mmap)
-    g.visited()
-
+        lines = f.readlines()
+        results = []
+        arr = list()
+        for line in lines:
+            result = line[:line.find(':')]
+            line = line[line.find(':') + 2:]
+            results.append(result)
+            numbers = []
+            while line:
+                space = line.find(' ')
+                if space == -1:
+                    space = line.find('\n')
+                if space == -1:
+                    break
+                numbers.append(int(line[:space]))
+                line = line[space+1:]
+            arr.append(numbers)
+        go_through_numbers(results, arr)
+            
 if __name__ == "__main__":
     main()
