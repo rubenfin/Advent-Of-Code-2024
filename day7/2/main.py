c68e50c1b7846a780da5f8138e4c4daa8e348dfa
rubenfin <rubenkfinnerud@gmail.com>
2025-06-22 09:23:29 +0200

    day7 ex2

--- day7/2/main.py
+++ day7/2/main.py
@@ -1,23 +1,52 @@
-from guard import Guard
+from itertools import product
 
-def find_guard(mmap: list, height: int, width: int)-> list:
-    for y in range(0, height):
-        for x in range(0, width):
-            if mmap[y][x] in '^>v<':
-                return (y, x)
+def do_operations(values: list, operations: list):
+    stack = values.copy()
+    stack.reverse()
+    for i in range(len(operations)):
+        num1 = stack.pop()
+        num2 = stack.pop()
+        if operations[i] == '+':
+            stack.append(num1 + num2)
+        elif operations[i] == '*':
+            stack.append(num1 * num2)
+        else:
+            stack.append((int(str(num1) + str(num2))))
+    return stack[0]
+
+
+def calculate(target: int, values: list)-> int:
+    n = len(values)
+    if n == 0:
+        return 0
+    
+    for ops in product(['+', '*', '||'], repeat=n-1):
+        result = do_operations(values, list(ops))
+        if result == target:
+            return result
+    return 0
+
+def go_through_numbers(results: list, arr: list):
+    sum = 0
+    for i in range(len(results)):
+        target = results[i]
+        numbers = arr[i]
+        sum += calculate(target, numbers)
+    print(sum)
 
 def main():
-    mmap = []
     with open("../input.txt") as f:
-        mmap = f.read().split('\n')
-    mmap = [list(row) for row in mmap]
-    height = len(mmap)
-    width = len(mmap[0])
-    g_location = find_guard(mmap, height, width)
-    y, x = g_location[0], g_location[1]
-    g = Guard(mmap[y][x], g_location,height, width)
-    g.move(mmap)
-    g.visited()
+        lines = f.readlines()
+        results = []
+        arr = []
+        for line in lines:
+            if ':' not in line:
+                continue
+            result_str, numbers_str = line.strip().split(':')
+            results.append(int(result_str.strip()))
+            numbers = list(map(int, numbers_str.strip().split()))
+            arr.append(numbers)
+        go_through_numbers(results, arr)
 
 if __name__ == "__main__":
     main()
