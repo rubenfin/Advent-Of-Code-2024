48294f2b8c3e484310bd5de6a9c662316ed0efe1
rubenfin <rubenkfinnerud@gmail.com>
2025-07-18 15:38:49 +0200

    day9 updated to lists

--- day9/1/main.py
+++ day9/1/main.py
@@ -1,31 +1,32 @@
 def main():
-    storage = ""
+    storage = []
     with open("../input.txt") as f:
-        line = f.readline()
+        line = f.readline().strip()
+    
     id = 0
     for n in range(len(line)):
         if n % 2 == 0:
-            storage += str(id) * int(line[n])
-            id+=1
+            storage.extend([id] * int(line[n]))
+            id += 1
         else:
-            storage += '.' * int(line[n])
+            storage.extend(['.'] * int(line[n]))
+    
     begin = 0
     end = len(storage) - 1
-    res = int(0)
+    res = 0
     
     while begin <= end:
         if storage[begin] != '.':
-            res += begin * int(storage[begin])
+            res += begin * storage[begin]
         else:
             while end > 0 and storage[end] == '.':
-                end-=1
-            print(begin, end)
-            print(storage[begin], storage[end])
+                end -= 1
             if begin > end:
                 break
-            res += begin * int(storage[end])
-            end-=1
-        begin+=1
+            res += begin * storage[end]
+            end -= 1
+        begin += 1
+    
     print(res)
 
 if __name__ == "__main__":
