b201aacf68804568b33e3e4462e6cc4ce7978a41
rubenfin <rubenkfinnerud@gmail.com>
2025-08-18 14:10:56 +0200

    advent of code day10

---
+++ day10/1/main.py
@@ -0,0 +1,31 @@
+
+def main():
+    with open("../input.txt") as f:
+         lines = [line.rstrip('\n') for line in f]
+    h = len(lines)
+    w = len(lines[0])
+    trail = [[int(lines[y][x]) for x in range(w)] for y in range(h)]
+    res = 0
+    def find_trail(y, x, elevation):
+        nonlocal total_trails
+        if y >= h or y < 0 or x >= w or x < 0:
+            return
+        if trail[y][x] != elevation:
+            return
+        if elevation == 9:
+            total_trails.add((y,x))
+            return
+        find_trail(y + 1, x, elevation+1)
+        find_trail(y - 1, x, elevation+1)
+        find_trail(y, x+1, elevation+1)
+        find_trail(y, x-1, elevation+1)
+    for y in range(h):
+        for x in range(w):
+            if trail[y][x] == 0:
+                total_trails = set()
+                find_trail(y, x, 0)
+                res += len(total_trails)
+    print(res)
+    return res
+if __name__ == "__main__":
+    main()
